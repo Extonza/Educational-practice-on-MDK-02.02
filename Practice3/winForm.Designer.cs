e4c4a6df00f76189d8accd67bc364213841350f1
Extonza <100854581+Extonza@users.noreply.github.com>
2023-04-15 20:31:25 +0700

    Выполнение практики по 02.02

---
+++ Practice3/winForm.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace Practice3
+{
+    partial class winForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbResult = new System.Windows.Forms.TextBox();
+            this.bWhile = new System.Windows.Forms.Button();
+            this.bFor = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(103, 198);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "f(x)=";
+            // 
+            // tbResult
+            // 
+            this.tbResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbResult.Location = new System.Drawing.Point(149, 195);
+            this.tbResult.Multiline = true;
+            this.tbResult.Name = "tbResult";
+            this.tbResult.ReadOnly = true;
+            this.tbResult.Size = new System.Drawing.Size(206, 28);
+            this.tbResult.TabIndex = 2;
+            // 
+            // bWhile
+            // 
+            this.bWhile.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.bWhile.Location = new System.Drawing.Point(99, 252);
+            this.bWhile.Name = "bWhile";
+            this.bWhile.Size = new System.Drawing.Size(125, 55);
+            this.bWhile.TabIndex = 3;
+            this.bWhile.Text = "Вычисление через While";
+            this.bWhile.UseVisualStyleBackColor = true;
+            this.bWhile.Click += new System.EventHandler(this.bWhile_Click);
+            // 
+            // bFor
+            // 
+            this.bFor.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.bFor.Location = new System.Drawing.Point(230, 252);
+            this.bFor.Name = "bFor";
+            this.bFor.Size = new System.Drawing.Size(125, 55);
+            this.bFor.TabIndex = 4;
+            this.bFor.Text = "Вычисление через for";
+            this.bFor.UseVisualStyleBackColor = true;
+            this.bFor.Click += new System.EventHandler(this.bFor_Click);
+            // 
+            // button1
+            // 
+            this.button1.Image = global::Practice3.Properties.Resources.icon;
+            this.button1.Location = new System.Drawing.Point(617, 82);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Practice3.Properties.Resources.уравнение;
+            this.pictureBox1.Location = new System.Drawing.Point(13, 39);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(436, 127);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Practice3.Properties.Resources.icon;
+            this.pictureBox2.Location = new System.Drawing.Point(417, 5);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(32, 28);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // winForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(461, 327);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.bFor);
+            this.Controls.Add(this.bWhile);
+            this.Controls.Add(this.tbResult);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "winForm";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbResult;
+        private System.Windows.Forms.Button bWhile;
+        private System.Windows.Forms.Button bFor;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ Practice3/winForm.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace Practice3
+{
+    public partial class winForm : Form
+    {
+        public winForm()
+        {
+            InitializeComponent();
+        }
+
+        public double x1 = 2;
+        public double a = 3.5;
+        public double ep = 2.718;
+
+        public double xn1 = 7;
+        public double xn2 = 29;
+
+        public double dx1 = 0.5;
+        public double dx2 = 3;
+
+        private void bWhile_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                tbResult.Clear();
+                while (x1 <= xn1)
+                {
+                    double result = (a * x1) * (1 + (a * Math.Pow(ep, -x1)));
+                    tbResult.Text = result.ToString();
+                    x1 = x1 + dx1;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка вычислений", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void bFor_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                tbResult.Clear();
+                for (x1 = 2; x1 <= xn2; x1 = x1 + dx2)
+                {
+                    double result = (a * x1) * (1 + (a * Math.Pow(ep, -x1)));
+                    tbResult.Text = result.ToString();
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка вычислений", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            Process.Start(@"E:\УП ПМ.02\День 3\Practice3\Practice3\Resources\Руководство пользователя.pdf");
+        }
+    }
+}
+
+
+
+
+
+
